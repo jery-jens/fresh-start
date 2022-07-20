7d8404e52f8d53730d204b361187249fa7f0b655
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-07-20 11:17:06 +0200

    Update shop.js

---
+++ js/shop.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    /**
+     * Images
+     */
+
+    const shownImage = document.querySelector(".product-image");
+    const images = document.querySelectorAll(".product-small-image");
+
+    images.forEach((image) => {
+        image.addEventListener("click", () => {
+            const style = image.currentStyle || window.getComputedStyle(image, false);
+            const url = style.backgroundImage.slice(4, -1).replace(/"/g, "");
+            console.log(url);
+        });
+    });
+
+    /**
+     * Selector
+     */
+});
