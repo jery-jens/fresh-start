6a9432c10d02fd7006212e56126b155003f5365a
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-08-08 16:39:47 +0200

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -89,6 +89,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.querySelector("form");
 
     form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        
         if (price === 0) {
             document.querySelector(".required").style.display = "block";
         } else {
